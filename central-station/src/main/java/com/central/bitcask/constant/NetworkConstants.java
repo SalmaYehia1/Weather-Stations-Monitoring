0dd9e28b432f0324becf272eb492b325e5c306ca
Salma Yehia <es-salma.yehia2027@alexu.edu.eg>
2026-05-22 18:04:06 +0300

    updated

--- central-station/src/main/java/com/central/bitcask/constant/NetworkConstants.java
+++ central-station/src/main/java/com/central/bitcask/constant/NetworkConstants.java
@@ -6,7 +6,7 @@ public final class NetworkConstants {
 
     public static final String KAFKA_SERVER = "localhost:9092";
     public static final String WEATHER_TOPIC = "weather-data";
-    public static final String CONSUMER_GROUP_ID = "central-station-group";
-
+// NetworkConstants.java
+public static final String CONSUMER_GROUP_ID = "central-station-group-v2"; 
     public static final String STATION_KEY_PREFIX = "station_id:";
 }
